e15a1b92373ce8c5352b31e843c3f3c0bcca917a
kant003 <gonzalezm.angel@gmail.com>
2022-10-27 09:13:20 +0200

    create structure and styles

---
+++ main.js
@@ -0,0 +1,9 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+
+document.getElementById('button')
+      .addEventListener('click', 
+      () => alert('Deja de pulsarme')
+)
+
+      
